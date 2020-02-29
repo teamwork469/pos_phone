6d19973fcc6d04e94daa35cfb397bd4e149b5796
senghoung469 <workteamtoday@gmail.com>
2020-02-29 12:56:48 +0700

    as

--- application/views/dashboard/sidebar.php
+++ application/views/dashboard/sidebar.php
@@ -39,7 +39,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="<?=site_url('Userlogin')?>" class="nav-link">
             <i class="fa fa-sign-out" aria-hidden="true"></i>
               <p>Logout</p>
             </a>
