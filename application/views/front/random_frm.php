e2ef3cbc555a0508aa05144a3d8b6f43e412198a
senghoung469 <workteamtoday@gmail.com>
2020-03-01 21:15:58 +0700

    update_link

--- application/views/front/random_frm.php
+++ application/views/front/random_frm.php
@@ -4,15 +4,15 @@
   <title>Random</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="<?=base_url('')?>dist/css/bootstrap.min.css">
+  <script src="<?=base_url('')?>dist/js/jquery2.min.js"></script>
   
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="<?=base_url('')?>dist/js/jquery.min.js"></script>
+  <script src="<?=base_url('')?>dist/js/popper.min.js"></script>
+  <script src="<?=base_url('')?>dist/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="<?=base_url('')?>dist/css/font-awesome.min.css">
 
-<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+<script src="<?=base_url('')?>dist/js/modernizr2.js"></script>
 
   <style type="text/css">
     #txt_insert_phone{
